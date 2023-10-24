af394577b8cc9c0d371c2b8da218a3812f19857f
Jesse-Driskill <jesse@driskill.com>
2023-10-23 18:54:09 -0700

    commit

--- index.js
+++ index.js
@@ -15,24 +15,90 @@ app.get('/', (req, res) => {
 
 mongoose.connect(process.env.MONGO_URI);
 
+
+//
 const accountSchema = new mongoose.Schema({
   username: {required: true, type: String}
 });
 
 const Account = new mongoose.model("Account", accountSchema);
+//
+
+//
+const exerciseSchema = new mongoose.Schema({
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  userID: {type: String, required: true},
+  date: String
+});
+
+const Exercise = new mongoose.model("Exercise", exerciseSchema);
+//
 
 app.get('/api/users', (req, res) => {
   Account.find().then((accounts) => {
 
-    res.json({users: accounts});
+    res.json(accounts);
   
   }).catch((err) => {console.log(err)});
 });
 
+Account.deleteMany({}).then((result) => console.log(result)).catch((err) => console.error(err));
+Exercise.deleteMany({}).then((result) => console.log(result)).catch((err) => console.error(err));
+
 app.post('/api/users', (req, res) => {
   let submittedUser = new Account({username: req.body.username});
-  submittedUser.save().then((user) => {console.log(user)}).catch((err) => console.log(err));
-  res.json({status: "User submitted successfully"});
+  submittedUser.save().then((user) => {
+
+    console.log(user)
+    res.json({username: user.username, _id: user._id});
+  
+  }).catch((err) => console.log(err));
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  let exerciseDuration = req.body.duration;
+  let exerciseDescription = req.body.description;
+  let exerciseUserID = req.params._id;
+
+  let exerciseDate = new Date(req.body.date).toUTCString().slice(0,16);
+  if (exerciseDate == "Invalid Date") {
+    exerciseDate = new Date().toUTCString().slice(0,16);
+  }
+
+  let submittedExercise = new Exercise({
+    duration: exerciseDuration,
+    description: exerciseDescription,
+    userID: exerciseUserID,
+    date: exerciseDate
+  });
+
+  submittedExercise.save();
+  console.log(submittedExercise);
+
+  Account.findOne({_id: exerciseUserID}).then((user) => {
+    res.json({_id: user._id, username: user.username, 
+      date: submittedExercise.date, 
+      duration: submittedExercise.duration, 
+      description: submittedExercise.description});
+  }).catch((err) => console.log(err));
+
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  Account.findOne({_id: req.params._id}).then((user) => {
+
+    Exercise.find({userID: req.params._id}).then((exercises) => {
+      res.json({
+        username: user.username,
+        count: exercises.length,
+        _id: user._id,
+        log: exercises
+      });
+    }).catch((err) => console.log(err));
+    
+  }).catch((err) => console.error(err));
+
 });
 
 
